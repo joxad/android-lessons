42b8a5ae18c5361d62485d5a20a8bd6c88649b6a
joxad <jox.david@gmail.com>
2017-10-05 13:33:35 +0200

    [Initialisation] Creation of your first activity (empty for now)

---
+++ app/src/main/java/io/novajox/androidlessons/ActivityMain.java
@@ -0,0 +1,12 @@
+package io.novajox.androidlessons;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jocelyn on 05/10/2017.
+ */
+
+public class ActivityMain extends AppCompatActivity {
+
+
+}
