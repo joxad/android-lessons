61d5f22247169abf40bdbda2fd74d73db9d2ebc8
joxad <jox.david@gmail.com>
2017-10-05 13:41:20 +0200

    Discover the lifecyle of an activity [Log] Create Log in your application using Log.d for debug. This will be visible in LogCat in AndroidStudio

--- app/src/main/java/io/novajox/androidlessons/ActivityMain.java
+++ app/src/main/java/io/novajox/androidlessons/ActivityMain.java
@@ -1,12 +1,40 @@
 package io.novajox.androidlessons;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
- * Created by Jocelyn on 05/10/2017.
+ * {@link ActivityMain} is the first screen you see when you start the application.
+ * This is done in the src/main/AndroidManifest.xml
  */
-
 public class ActivityMain extends AppCompatActivity {
 
+    // this is a way to create a unique tag for all your activity
+    private final static String TAG = ActivityMain.class.getSimpleName();
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
 }
