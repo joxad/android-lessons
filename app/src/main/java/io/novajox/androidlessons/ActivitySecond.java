9c3a1f998762d547eec7bfc0ec75beeface92450
joxad <jox.david@gmail.com>
2017-10-05 13:48:53 +0200

    Create a second activity [Intent] Use the intent to create a link to an other activity

--- app/src/main/java/io/novajox/androidlessons/ActivitySecond.java
+++ app/src/main/java/io/novajox/androidlessons/ActivitySecond.java
@@ -1,24 +1,18 @@
 package io.novajox.androidlessons;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-/**
- * {@link ActivityMain} is the first screen you see when you start the application.
- * This is done in the src/main/AndroidManifest.xml
- */
-public class ActivityMain extends AppCompatActivity {
+public class ActivitySecond extends AppCompatActivity {
 
     // this is a way to create a unique tag for all your activity
-    private final static String TAG = ActivityMain.class.getSimpleName();
+    private final static String TAG = ActivitySecond.class.getSimpleName();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, ActivitySecond.class));
         Log.d(TAG, "onCreate");
     }
 
