f39126e336bfc700c352726a8a6449325364e475
joxad <jox.david@gmail.com>
2017-10-09 19:38:36 +0200

    This step will show how to call ws with info from edit text

--- app/src/main/java/io/novajox/androidlessons/app/ActivitySyncGitRepos.java
+++ app/src/main/java/io/novajox/androidlessons/app/ActivitySyncGitRepos.java
@@ -1,19 +1,18 @@
 package io.novajox.androidlessons.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
-import io.novajox.androidlessons.NavigationManager;
 import io.novajox.androidlessons.R;
 import io.novajox.androidlessons.data.GitHubManager;
 import io.novajox.androidlessons.data.model.Repo;
@@ -22,25 +21,26 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 /**
- * {@link ActivityMain} is the first screen you see when you start the application.
+ * {@link ActivitySyncGitRepos} is the first screen you see when you start the application.
  * This is done in the src/main/AndroidManifest.xml
  */
-public class ActivityMain extends AppCompatActivity {
+public class ActivitySyncGitRepos extends AppCompatActivity {
 
     // this is a way to create a unique tag for all your activity
-    private final static String TAG = ActivityMain.class.getSimpleName();
-    private TextView textView;
+    private final static String TAG = ActivitySyncGitRepos.class.getSimpleName();
+    private EditText etUser;
     private ProgressBar progressBar;
+    private TextView textView;
     private Button bt;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-        textView = (TextView) findViewById(R.id.tv_main_activity);
+        setContentView(R.layout.activity_sync_git_repositories);
+        textView = (TextView) findViewById(R.id.activity_ws_tv_result);
+        etUser = (EditText) findViewById(R.id.et_user);
         bt = (Button) findViewById(R.id.activity_main_bt);
         progressBar = (ProgressBar) findViewById(R.id.activity_ws_progress);
-        textView.setText(R.string.text_updated);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,24 +55,27 @@ public class ActivityMain extends AppCompatActivity {
     private void callWs() {
         progressBar.setVisibility(View.VISIBLE);
         bt.setVisibility(View.GONE);
-        GitHubManager.INSTANCE.getRepositories("octocat").enqueue(new Callback<List<Repo>>() {
+        GitHubManager.INSTANCE.getRepositories(etUser.getText().toString()).enqueue(new Callback<List<Repo>>() {
             @Override
             public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
                 Log.d(TAG, "response ok");
                 List<Repo> repos = response.body();
-                textView.setText(repos.get(0).getName());
                 progressBar.setVisibility(View.GONE);
-                Toast.makeText(ActivityMain.this, "Success", Toast.LENGTH_LONG).show();
+                Toast.makeText(ActivitySyncGitRepos.this, "Success", Toast.LENGTH_LONG).show();
+                if (repos != null) {
+                    textView.setText(repos.size() + " repos have been found !");
+                } else {
+                    textView.setText("Nothing has been found !");
 
-                NavigationManager.goToActivity2(ActivityMain.this,repos.get(0));
-                finish();
+                }
             }
 
             @Override
             public void onFailure(Call<List<Repo>> call, Throwable t) {
                 Log.d(TAG, "response fail");
                 progressBar.setVisibility(View.GONE);
-                Toast.makeText(ActivityMain.this, "fail", Toast.LENGTH_LONG).show();
+                Toast.makeText(ActivitySyncGitRepos.this, "fail", Toast.LENGTH_LONG).show();
+                bt.setVisibility(View.VISIBLE);
 
             }
         });
